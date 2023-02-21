8f5a988bcaa26cbf575678a9eea8eb43b9423e7d
LawrenceEzealor <ezealor20@gmail.com>
2023-02-21 06:53:01 -0800

    new update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * main - print_putchar
- * is written by mbah
- * Return: 0
+ * main - print _putchar
+ * Return: 0 (success)
  */
 
 int main(void)
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,5 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c)
 
-int_putchar(char);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int_islower(int c);
-int_isalpha(int c);
-int_print_sign(int n);
-int_abs(int);
-int_print_last_digit(int);
-void jack_table(void);
-int add(int, int)
-void print_to_98(int n);
+#endif
