e04a97ebffb9c51dc2d3e86a37385642f98bda30
LawrenceEzealor <ezealor20@gmail.com>
2023-04-10 04:23:14 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,15 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "main.h"
 /**
 * read_textfile - check the code for Holberton School students.
 * @filename: The file to read and write
-* @letters: The number of letters to read and write.
-* Return: The letters printed
+* @letters: The  number of letters to read and write.
+* Return: letters printed
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t nletters;
-	int file;
 	char *text;
+	int file;
 
 	if (!filename)
 		return (0);
