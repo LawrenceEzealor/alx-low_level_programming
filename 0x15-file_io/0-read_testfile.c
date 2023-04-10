492415613c50ecd0fb50f364b97e0d27ad8f70c3
LawrenceEzealor <ezealor20@gmail.com>
2023-04-10 04:14:32 -0700

    0-read_testfile.c

--- 0x15-file_io/0-read_testfile.c
+++ 0x15-file_io/0-read_testfile.c
@@ -6,7 +6,7 @@
 * read_textfile - check the code for Holberton School students.
 * @filename: The file to read and write
 * @letters: The number of letters to read and write.
-* Return:The letters printed
+* Return: The letters printed
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
